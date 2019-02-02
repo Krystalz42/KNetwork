1cc38c480cc31bdadc397527ae7841330185b5e8
Krystalz42 <aroulin@student.42.fr>
2019-02-02 19:47:40 +0100

    some fix

--- KNW/ServerTCP.cpp
+++ KNW/ServerTCP.cpp
@@ -71,58 +71,12 @@ namespace KNW {
 		iotcp->readSocketHeader();
 	}
 
-	bool ClientTCP::isConnect() const {
-		return socket_.is_open();
-	}
-
 	void ConnectionTCP::sendData(std::string data) {
 		iotcp->writeSocket(std::move(data));
 	}
 
 	ConnectionTCP::~ConnectionTCP() {
 		std::cout << "~ConnectionTCP" << std::endl;
-	}
-
-	void ClientTCP::connect(std::string dns, std::string port) {
-		std::cout << "socket : " << socket_.is_open() << std::endl;
-		try {
-			tcp::resolver::query query(dns, port);
-			tcp::resolver::iterator it = resolver.resolve(query);
-			boost::asio::connect(socket_, it);
-			iotcp = std::make_unique<IOTCP>(
-					dataTCP_,
-					std::move(socket_),
-					std::bind(&DataTCP::sendDataToCallback, std::ref(dataTCP_),
-							  std::placeholders::_1, std::placeholders::_2));
-			iotcp->readSocketHeader();
-			thread = boost::thread(
-					boost::bind(&boost::asio::io_service::run, &io));
-			thread.detach();
-		} catch (std::exception &exception) {
-			std::cout << exception.what() << std::endl;
-		}
-
-	}
-
-	ClientTCP::ClientTCP()
-			: resolver(io),
-			  socket_(io) {
-		std::cout << "ClientTCP" << std::endl;
-	}
-
-
-	ClientTCP::~ClientTCP() {
-		try {
-			if (socket_.is_open()) {
-				socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-				socket_.close();
-			}
-		} catch (std::exception const &e) {
-			std::cout << e.what() << std::endl;
-		}
-
-		thread.interrupt();
-		io.stop();
 	}
 
 }
